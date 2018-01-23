4a714dca0103f1488a1ea5ffe62b1c9000b4ff8c
汤波 <478185912@qq.com>
2018-01-23 16:23:03 +0800

    添加发送程序测试端

---
+++ Test.Client/Test.Client/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Test.Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.edit_ip = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.edit_port = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.memoEdit2 = new DevExpress.XtraEditors.MemoEdit();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btn_send = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btn_connnect = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem6 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edit_ip.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edit_port.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.btn_connnect);
+            this.layoutControl1.Controls.Add(this.btn_send);
+            this.layoutControl1.Controls.Add(this.memoEdit2);
+            this.layoutControl1.Controls.Add(this.memoEdit1);
+            this.layoutControl1.Controls.Add(this.edit_port);
+            this.layoutControl1.Controls.Add(this.edit_ip);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(926, 260, 250, 350);
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(542, 498);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3,
+            this.layoutControlItem4,
+            this.layoutControlItem5,
+            this.layoutControlItem6});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(542, 498);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // edit_ip
+            // 
+            this.edit_ip.EditValue = "127.0.0.1";
+            this.edit_ip.Location = new System.Drawing.Point(39, 12);
+            this.edit_ip.Name = "edit_ip";
+            this.edit_ip.Size = new System.Drawing.Size(191, 20);
+            this.edit_ip.StyleController = this.layoutControl1;
+            this.edit_ip.TabIndex = 4;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.edit_ip;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(222, 26);
+            this.layoutControlItem1.Text = "IP";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(23, 14);
+            // 
+            // edit_port
+            // 
+            this.edit_port.EditValue = "13909";
+            this.edit_port.Location = new System.Drawing.Point(261, 12);
+            this.edit_port.Name = "edit_port";
+            this.edit_port.Size = new System.Drawing.Size(176, 20);
+            this.edit_port.StyleController = this.layoutControl1;
+            this.edit_port.TabIndex = 5;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.edit_port;
+            this.layoutControlItem2.Location = new System.Drawing.Point(222, 0);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(207, 26);
+            this.layoutControlItem2.Text = "Port";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(23, 14);
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Location = new System.Drawing.Point(12, 38);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Size = new System.Drawing.Size(518, 220);
+            this.memoEdit1.StyleController = this.layoutControl1;
+            this.memoEdit1.TabIndex = 6;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.memoEdit1;
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 26);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(522, 224);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // memoEdit2
+            // 
+            this.memoEdit2.EditValue = resources.GetString("memoEdit2.EditValue");
+            this.memoEdit2.Location = new System.Drawing.Point(12, 262);
+            this.memoEdit2.Name = "memoEdit2";
+            this.memoEdit2.Size = new System.Drawing.Size(518, 198);
+            this.memoEdit2.StyleController = this.layoutControl1;
+            this.memoEdit2.TabIndex = 7;
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.memoEdit2;
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 250);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(522, 202);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(12, 464);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(518, 22);
+            this.btn_send.StyleController = this.layoutControl1;
+            this.btn_send.TabIndex = 8;
+            this.btn_send.Text = "发送";
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.btn_send;
+            this.layoutControlItem5.Location = new System.Drawing.Point(0, 452);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(522, 26);
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem5.TextVisible = false;
+            // 
+            // btn_connnect
+            // 
+            this.btn_connnect.Location = new System.Drawing.Point(441, 12);
+            this.btn_connnect.Name = "btn_connnect";
+            this.btn_connnect.Size = new System.Drawing.Size(89, 22);
+            this.btn_connnect.StyleController = this.layoutControl1;
+            this.btn_connnect.TabIndex = 9;
+            this.btn_connnect.Text = "连接";
+            this.btn_connnect.Click += new System.EventHandler(this.btn_connnect_Click);
+            // 
+            // layoutControlItem6
+            // 
+            this.layoutControlItem6.Control = this.btn_connnect;
+            this.layoutControlItem6.Location = new System.Drawing.Point(429, 0);
+            this.layoutControlItem6.Name = "layoutControlItem6";
+            this.layoutControlItem6.Size = new System.Drawing.Size(93, 26);
+            this.layoutControlItem6.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem6.TextVisible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 498);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "Form1";
+            this.Text = "发送程序";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edit_ip.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edit_port.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraEditors.TextEdit edit_ip;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraEditors.TextEdit edit_port;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraEditors.MemoEdit memoEdit2;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraEditors.SimpleButton btn_send;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+        private DevExpress.XtraEditors.SimpleButton btn_connnect;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem6;
+    }
+}
+
---
+++ Test.Client/Test.Client/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using TCPHandler;
+
+namespace Test.Client
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        SocketClient smanager = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_connnect_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(edit_ip.Text) || string.IsNullOrWhiteSpace(edit_port.Text)) { MessageBox.Show("请先填入IP和Port"); return; }
+
+            if (Convert.ToInt16(edit_port.Text) <= 1000) { MessageBox.Show("端口号必须大于1000"); return; }
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(edit_ip.Text), Convert.ToInt16(edit_port.Text));
+
+            SocketError res = Connect(endPoint);
+
+            if (res == 0) { memoEdit1.MaskBox.AppendText("连接成功\r\n"); return; }
+
+            memoEdit1.MaskBox.AppendText("连接失败，错误码：" + res);
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(memoEdit2.Text)) { MessageBox.Show("请输入发送内容"); return; }
+
+            smanager.Send(ConvertMsg(memoEdit2.Text));
+        }
+
+        #region 方法
+
+        private byte[] ConvertMsg(string text)
+        {
+            string[] strs = text.Split(' ');
+
+            byte[] data = new byte[strs.Length];
+
+            for (int i = 0, length = strs.Length; i < length; i++)
+            {
+                data[i] = Convert.ToByte(strs[i], 16);
+            }
+
+            return data;
+        }
+
+        public SocketError Connect(IPEndPoint endPoint)
+        {
+            if (smanager != null && smanager.Connected) return SocketError.Success;
+
+            //创建连接对象, 连接到服务器  
+            smanager = new SocketClient(endPoint);
+
+            SocketError error = smanager.Connect();
+
+            if (error == SocketError.Success)
+            {
+                //连接成功后,就注册事件. 最好在成功后再注册.  
+                smanager.OnMsgReceived += Smanager_OnMsgReceived;
+                smanager.GetPackageLength += Smanager_GetPackageLength;
+                smanager.GetSendMessage += Smanager_GetSendMessage;
+                smanager.OnSended += Smanager_OnSended;
+            }
+            return error;
+        }
+
+        private void Smanager_OnSended(bool successorfalse)
+        {
+            SetMemoText(successorfalse ? "发送消息成功\r\n" : "发送消息失败\r\n");
+        }
+
+        private byte[] Smanager_GetSendMessage(string msg)
+        {
+            if (smanager == null || !smanager.Connected) { MessageBox.Show("未连接到服务端，请重新连接"); return null; };
+
+            byte[] sendBuffer = Encoding.UTF8.GetBytes(msg);
+
+            byte[] buff = new byte[sendBuffer.Length + 4];
+            Array.Copy(BitConverter.GetBytes(sendBuffer.Length), buff, 4);
+            Array.Copy(sendBuffer, 0, buff, 4, sendBuffer.Length);
+
+            return buff;
+        }
+
+        private int Smanager_GetPackageLength(byte[] data, out int headLength)
+        {
+            headLength = 4;
+            byte[] lenBytes = new byte[4];
+            Array.Copy(data, lenBytes, 4);
+            int packageLen = BitConverter.ToInt32(lenBytes, 0);
+            return packageLen;
+        }
+
+        private void Smanager_OnMsgReceived(byte[] info)
+        {
+            SetMemoText("接收到服务端的消息：" + Encoding.UTF8.GetString(info) + "\r\n");
+        }
+
+        private delegate void setMemoText(string str);
+
+        private void SetMemoText(string str)
+        {
+            if (memoEdit1.InvokeRequired)
+            {
+                BeginInvoke(new setMemoText(SetMemoText), str);
+            }
+            else
+            {
+                memoEdit1.MaskBox.AppendText(str);
+            }
+        }
+        #endregion
+    }
+}
