90da703525af68884e8e95f092b982f948725460
汤波 <478185912@qq.com>
2018-02-08 14:57:03 +0800

    增加组织机构视图

--- DQGJK.Web/DQGJK.Web/Controllers/DepartmentController.cs
+++ DQGJK.Web/DQGJK.Web/Controllers/DepartmentController.cs
@@ -3,22 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using DQGJK.Web.PageModels;
-using DQGJK.Models;
-using DQGJK.Web.Contexts;
-using System.Xml.Linq;
-using System.Collections;
-using Microsoft.AspNetCore.Hosting;
 
 namespace DQGJK.Web.Controllers
 {
-    public class HomeController : BaseController
+    public class DepartmentController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-
-
     }
 }
