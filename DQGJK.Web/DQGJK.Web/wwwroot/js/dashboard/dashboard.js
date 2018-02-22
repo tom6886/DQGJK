e37acbd392f10228f0e2550fc07a4e60879aab95
汤波 <478185912@qq.com>
2018-02-22 21:23:01 +0800

    添加js文件

---
+++ DQGJK.Web/DQGJK.Web/wwwroot/js/dashboard/dashboard.js
@@ -0,0 +1,36 @@
+﻿;
+(function ($) {
+    var globe = { ty: null, yy: null };
+
+    var priv = {
+        set: function ($this) {
+            $this.each(function (i, v) {
+                var func = fc[$(v).data('board')];
+
+                if (!func) { return true; }
+
+                func($(v));
+            });
+        }
+    };
+
+    var fc = {
+        t: function (e) {
+
+        },
+        y: function (e) {
+
+        }
+    }
+
+    $.fn.dashboard = function () {
+
+        var $this = this;
+
+        if ($this.length == 0) { return; }
+
+        priv.set($this);
+
+        return $this;
+    };
+})(jQuery);
