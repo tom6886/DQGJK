d231a7259431979342748a03a752858ea1b41592
汤波 <478185912@qq.com>
2018-03-28 06:18:04 +0800

    winform测试程序增加全局异常捕获

--- DQGJK.Winform/DQGJK.Winform/Program.cs
+++ DQGJK.Winform/DQGJK.Winform/Program.cs
@@ -18,6 +18,13 @@ namespace DQGJK.Winform
         {
             try
             {
+                //处理未捕获的异常   
+                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+                //处理UI线程异常   
+                Application.ThreadException += Application_ThreadException;
+                //处理非UI线程异常   
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException; ;
+
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
 
@@ -30,6 +37,18 @@ namespace DQGJK.Winform
             {
                 LogHelper.WriteLog("启动时报错", ex.Message, ex.StackTrace);
             }
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            LogHelper.WriteLog("发生非UI线程异常", ex.Message, ex.StackTrace);
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+
+            LogHelper.WriteLog("发生UI线程异常", e.Exception.Message, e.Exception.StackTrace);
         }
     }
 }
